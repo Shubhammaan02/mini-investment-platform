9f0b4df00af4cc8a3d3557c67b3b268697fecd7f
Shubhammaan02 <112062782+Shubhammaan02@users.noreply.github.com>
2025-12-04 13:03:03 +0530

    Change default database port to 3306

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,8 +8,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    // port: parseInt(process.env.DB_PORT) || '17724',
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 17724,
+    // port: process.env.DB_PORT,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
     dialect: 'mysql',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
 
