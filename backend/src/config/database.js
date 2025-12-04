5e39eb5efbeb3c00c574c33c487fa3b5e2a3ac6e
Shubhammaan02 <112062782+Shubhammaan02@users.noreply.github.com>
2025-12-04 11:08:58 +0530

    Refactor database configuration to use environment variables

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,13 +3,13 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'defaultdb',
-  process.env.DB_USER || 'avnadmin',
-  process.env.DB_PASSWORD || 'AVNS_Wu_7l2iGVv-gVTmPkS6',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'mysql-172eee34-maanshubham002-63fc.j.aivencloud.com',
-    port: parseInt(process.env.DB_PORT) || '17724',
-    // port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+    host: process.env.DB_HOST,
+    // port: parseInt(process.env.DB_PORT) || '17724',
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 17724,
     dialect: 'mysql',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
 
